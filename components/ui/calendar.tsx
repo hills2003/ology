81f4043631cada3dbe769eebbeddcd60ee21fefc
Hillary Victor <chillaryvictor@gmail.com>
2026-04-07 09:03:13 +0100

    chevron color fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -153,21 +153,27 @@ function Calendar({
         Chevron: ({ className, orientation, ...props }) => {
           if (orientation === "left") {
             return (
-              <ChevronLeftIcon className={cn("size-4", className)} {...props} />
+              <ChevronLeftIcon
+                className={cn("size-4 text-white", className)}
+                {...props}
+              />
             );
           }
 
           if (orientation === "right") {
             return (
               <ChevronRightIcon
-                className={cn("size-4", className)}
+                className={cn("size-4 text-white", className)}
                 {...props}
               />
             );
           }
 
           return (
-            <ChevronDownIcon className={cn("size-4", className)} {...props} />
+            <ChevronDownIcon
+              className={cn("size-4 text-white", className)}
+              {...props}
+            />
           );
         },
         DayButton: ({ ...props }) => (
